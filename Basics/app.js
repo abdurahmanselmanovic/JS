22cab25563eb50f0fa3dbc773e727ccb505016a6
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-07 00:17:29 +0200

    basics

---
+++ Basics/app.js
@@ -0,0 +1,8 @@
+// JavaScript je najpopularniji programski jezin na svetu
+
+// JavaScript je case-sensitive. (RAZLIKUJE VELIKA I MALA SLOVA)
+
+// Najlaksi nacin za prikazivanje podataka u JS je pomocu
+// document.write() komande
+
+// JavaScript je programski jezik sa najvise framework-ova
---
+++ Variables/app.js
@@ -0,0 +1,21 @@
+// U JavaScriptu postoji 3 nacina da deklarisemo varijablu
+
+// 1. nacin je koriscenjem keyword-a var
+// **NAPOMENA** varijable deklarisane koriscenjem var keyword-a mogu biti
+// redeklarisane i reinicijalizovane
+var prom;
+prom = 12;
+// 2. nacin je koriscenjem keyword-a let
+// **NAPOMENA** varijable deklarisane koriscenjem let keyword-a mogu biti
+// reinicijalizovane, a ne mogu biti redeklarisane (osim ako nisu u drugom scope-u)
+let prom1;
+prom1 = 12;
+// 3. nacin je koriscenjem const keyword-a
+// **NAPOMENA** varijable deklarisane const kljucnom reci ne mogu biti ni reinicijalizovane
+// ni redeklarisane
+const x = 12;
+
+// Imena varijabli mogu pocinjati:
+// 1. slovom
+// 2. donjom crtom
+// 3. dolar znakom $$$$$$$$$$$
