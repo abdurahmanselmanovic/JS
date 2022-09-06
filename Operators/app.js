ace3f0b2b04aea1cca7ed8cc62609397317bd233
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-06 21:21:49 +0200

    Operators in JS

---
+++ Operators/app.js
@@ -0,0 +1,42 @@
+// Aritmeticki operatori
+
+// + sabiranje
+// - oduzimanje
+// * mnozenje
+// ** stepenovanje
+// / deljenje
+// % ostatak pri deljenju
+// ++ povecanje za 1
+// -- smanjenje za 1
+
+// Operatori dodele vrednosti
+
+// = jednako
+// += na vec postojecu vrednost dodajemo broj
+// -= na vec postojecu vrednost oduzimamo broj
+// *= na vec postojecu vrednost mnozimo broj
+// /= na vec postojecu vrednost delimo broj
+
+// Stringovni operatori
+
+// Za spajanje strinogova mozemo koristiti + operator
+
+// Operatori poredjenja
+
+// == Proverava jednakost vrednosti
+// === Proverava jednakost vrednosti i tipa
+// !=  Proverava nejednakost vrednosti
+// !== Proverava nejednakost vrednosti ili nejednakost tipa
+// >  Proverava da li je prva promenljiva veca od druge
+// <  Proverava da li je prva promenljiva manja od druge
+// <= Proverava da li je prva promenljiva manja ili jednaka od druge
+// >= Proverava da li je prva promenljiva veca ili jednaka od druge
+
+// Logicki operatori
+
+// && (and)
+// || (ili)
+// ! (not)
+
+// Tipovni operatori
+// typeof Vraca tip promenljive
