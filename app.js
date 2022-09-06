6e5f4507a49a8405bfed8b407114fae0dec971a0
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-06 20:53:29 +0200

    JS Number metode

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+// Ako zelimo da neki realan broj prevedemo iz number tipa podatka u string koristimo
+// JS metodu .toString()
+
+let y = 12;
+console.log(typeof y.toString());
+
+// Ako zelimo da zaokruzimo neki realan broj na odreden broj decimala koristimo
+// JS metodu toPrecision()
+
 let x = 12.925922229222;
 console.log(x.toPrecision(4));
