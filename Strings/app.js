dcd651a71c0934a4caed30e2d0b5c1c116eb1d26
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-06 21:07:02 +0200

    stringovi

---
+++ Strings/app.js
@@ -0,0 +1,14 @@
+// U JavaScriptu mozemo korisiti za stringove:
+// 1. duple navodnike ("")
+// 2. single navodnike('')
+// 3. ili ove cudne (``)
+// Primeri:
+let prom1 = "Ovo je prvi primer";
+let prom2 = "Ovo je drugi primer";
+let prom3 = `Ovo je treci primer`;
+
+// Ukoliko zelimo da se recenica nastavlja u novom redu mozemo ubaciti backslash n (\n)
+
+let recenica1 =
+	"ovaj tekst se nalazi u prvom redu\novaj deo se nalazi u drugom redu\na ovaj deo u trecem redu";
+console.log(recenica1);
---
+++ Strings/methods.js
@@ -0,0 +1,14 @@
+// Ukoliko zelimo da dobijemo duzinu nekog stringa koristimo metodu .length
+
+let recenica = "Ovo je recenica";
+console.log(recenica.length);
+
+// Ukoliko zelimo da recenicu prevedemo u velika slova koristimo metodu .toUpperCase()
+
+let text = "Hello world";
+console.log(text.toUpperCase());
+
+// Ukoliko zelimo da recenicu prevedemo u mala slova koristimo metodu .toLowerCase()
+
+let text1 = "HELLO WORLD";
+console.log(text1.toLowerCase());
