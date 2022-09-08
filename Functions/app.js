a9521f9b4a877778b77ad9b9163a632827079d31
abdurahman <selmanovicabdurrahman@gmail.com>
2022-09-08 13:29:39 +0200

    functions in JS

---
+++ Functions/app.js
@@ -0,0 +1,18 @@
+// FUNKCIJE
+
+// funkcije predstavljaju blok koda koji se izvrsava samo prilikom pozivanja.
+
+// Nacini zapisivanja funckija:
+// 1. koriscenjem keyword function
+// 2. nacin:
+// const myFunction = function () {
+// 	// blok koda
+// };
+// 3. nacin
+// const arrowFunction = () => {
+// 	// sintaksa
+// };
+
+// svaka funckija u JS moze imati (a ne mora) parametre
+// funkcija se poziva na sledeci nacin:
+imefunkcije();
