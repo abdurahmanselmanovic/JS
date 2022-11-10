604b16487d0607ffb6660db6bd2283c0aee950f0
abdurahman <selmanovicabdurrahman@gmail.com>
2022-11-10 22:21:10 +0100

    Arrays

---
+++ Arrays/app.js
@@ -0,0 +1,5 @@
+// Arrays ili nizovi su neprimitivnog tipa podatka koji mogu sadrzati vise od jedne vrednosti
+
+// Kreiranje niza:
+const someName = [];
+const someName1 = new Array("");
---
+++ Arrays/methods.js
@@ -0,0 +1,25 @@
+// Za konvertovanje niza u string korisitimo metodu toString()
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.toString());
+
+// pop() metoda sklanja poslednji element u nizu
+
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits1.pop());
+
+// push() metoda dodaje novi element na zadnje mesto niza
+const fruits3 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits3.push("Kiwi"));
+
+// shift() metoda sklanja prvi element niza
+
+const fruits4 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits4.shift());
+
+// unshift() metoda dodaje novi element na prvo mesto
+
+const fruits5 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits5.unshift("Lemon"));
+
+// length metoda nam daje broj elemenata u nizu - 1
